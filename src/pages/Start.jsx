8f126f7be90d2e78d81a7dc5e4db70f23cdb216d
Jhoider_Rua <coblack421@gmail.com>
2024-11-19 20:34:46 +0100

    Arreglando el footer de inicio

--- src/pages/Start.jsx
+++ src/pages/Start.jsx
@@ -1,16 +1,54 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+//Style
+import startModule from "./Start.module.css";
+//Imagen
+import Fondojfif from "../assets/Fondo.jfif";
 //Components
-import Footer from "../components/Footer";
-import Navbar from "../components/NavBar";
+import Card from "../components/Card";
+//Services
+import getProducts from "../services/getProducts";
 
 const start = () => {
+  const [carsList, setCarList] = useState([]);
+
+  useEffect(() => {
+    getProducts().then((data) => {
+      setCarList(data);
+    });
+  }, []);
+
   return (
     <>
-      <Navbar />
-      <main>
-        <h1>Inicio</h1>
+      <main className={startModule.main}>
+        <section className={startModule.header}>
+          <img src={Fondojfif} alt="" className={startModule.img} />
+        </section>
+        <section className={startModule.products}>
+          {carsList
+            .map(
+              (
+                { id, title, price, description, category, image, rating },
+                index
+              ) => (
+                <Card
+                  key={id}
+                  ID={id}
+                  title={title}
+                  price={price}
+                  description={description}
+                  category={category}
+                  image={image}
+                  rating={rating}
+                />
+              )
+            )
+            .slice(0, 4)}
+        </section>
+
+        <section className={startModule.service}>
+          <article className={startModule.article}></article>
+        </section>
       </main>
-      <Footer />
     </>
   );
 };
