336796c89c1df2f4c29c3d10afce8338468304a9
GuzGuz08 <andres.escudero317@gmail.com>
2024-11-28 21:34:49 -0500

    se sube el metodo crear

--- BackEnd/src/routes/ProductsRouter.js
+++ BackEnd/src/routes/ProductsRouter.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { getProducts } from "../controller/ProductsController.js";
+import { createProducts } from "../controller/ProductsController.js";
 
 const productsRouter = Router();
 
 productsRouter.get("/getProducts", getProducts);
+productsRouter.post("/createProducts", createProducts)
 
 export default productsRouter;
