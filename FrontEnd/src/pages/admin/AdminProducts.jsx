e70e7b0444b5aa2b2491a0c485c3b1de62839e5d
leiderstewartmiranda <mirandaleider4@gmail.com>
2024-12-04 14:51:23 -0500

    Se corrigen errores

--- FrontEnd/src/pages/admin/AdminProducts.jsx
+++ FrontEnd/src/pages/admin/AdminProducts.jsx
@@ -17,14 +17,13 @@ import { useAutoAnimate } from "@formkit/auto-animate/react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchProducts } from "../../redux/slices/productsSlice";
 
-
 const AdminProducts = () => {
   //Redux - userSlice
   const { products, loading } = useSelector((state) => state.products);
-  const dispatch = useDispatch();
   const { token, email, name, role, avatar } = useSelector(
     (state) => state.user
   );
+  const dispatch = useDispatch();
 
   //Elementos para la paginacion
   const [currentPage, setCurrentPage] = useState(1);
@@ -33,15 +32,12 @@ const AdminProducts = () => {
 
   //Auto-animate
   const [parent] = useAutoAnimate();
-  const { products, loading 
-    
-  } = useSelector((state) => state.products);
 
   useEffect(() => {
     if (loading == "idle") dispatch(fetchProducts());
   }, [loading, dispatch]);
 
-  return (      
+  return (
     <Container>
       <header className={adminproductsModule.header}>
         <span className={adminproductsModule.header_title}>Dashboard</span>
@@ -59,8 +55,11 @@ const AdminProducts = () => {
         <h1 className={adminproductsModule.products_title}>Productos</h1>
         <section className={adminproductsModule.products_section}>
           <article>
-          <FormCreateProduct T={"Agregar producto"} TxtBtn={"Agregar nuevo producto"} TxtBtnIn={"Crear producto"}/>
-            {/* Aca va la barra de buscar y el boton de agregar */}
+            <FormCreateProduct
+              T={"Agregar producto"}
+              TxtBtn={"Agregar nuevo producto"}
+              TxtBtnIn={"Crear producto"}
+            />
           </article>
           <article>
             <table className={adminproductsModule.table}>
